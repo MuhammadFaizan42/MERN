d5f811573a6ffdbeb0ab1f9286faf3f609c8180b
MuhammadFaizan42 <89015907+MuhammadFaizan42@users.noreply.github.com>
2024-08-12 17:04:46 +0500

    Update Navbar.jsx

--- Frontend/src/components/layout/Navbar.jsx
+++ Frontend/src/components/layout/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
     e.preventDefault();
     try {
       const { data } = await axios.get(
-        "http://localhost:4000/api/logout",
+        "https://mern-pq6d.vercel.app/api/logout",
         { withCredentials: true }
       );
       setIsAuthenticated(false);
